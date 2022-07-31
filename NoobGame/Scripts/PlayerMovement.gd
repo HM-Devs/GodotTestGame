b481858922c35842651a7fc767024969cb36e402
Humster <hamzahmalik1@hotmail.com>
2022-07-31 19:53:30 +0100

    movement updates
    added move_and_slide replacement to move_and_collide for smoother movement + max speed, friction and acceleration for much smoother movements. have added some sample static body ranges to test movement against physical objects

--- NoobGame/Scripts/PlayerMovement.gd
+++ NoobGame/Scripts/PlayerMovement.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var acceleration = Vector2.ZERO
+const max_speed = 80
+const acceleration = 500
+const friction = 500
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("move_d") - Input.get_action_strength("move_a")
+	input_vector.y = Input.get_action_strength("move_s") - Input.get_action_strength("move_w")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
+	
